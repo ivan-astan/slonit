4a0c06dcefa9564c3f7c980ea41d9a50e4cbc321
ivan-astan <astanin.i.a@gmail.com>
2025-01-25 18:54:57 +0300

    myfilter function

---
+++ filter.js
@@ -0,0 +1,11 @@
+function myFilter(array, callback) {
+    const result = [];
+    
+    for (let i = 0; i < array.length; i++) {
+      if (callback(array[i], i, array)) {
+        result.push(array[i]);
+      }
+    }
+  
+    return result;
+  }
