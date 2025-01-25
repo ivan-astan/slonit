fda08ef1f3ff3f64f6c515b0d113ccc4298dba56
ivan-astan <astanin.i.a@gmail.com>
2025-01-25 18:51:56 +0300

    calculate function

---
+++ calculate.js
@@ -0,0 +1,10 @@
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+const calculate = function (a, b, operation) {
+  return operation(a, b);
+};
